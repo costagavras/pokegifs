ea8fc5fa67c1066d37429a361f1fa4344cefc937
costagavras <cgavras@yahoo.ca>
2018-10-23 17:56:43 -0400

    all pokemonz apprehended

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'typhoeus'
 gem 'httparty'
+gem 'pry'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -4,19 +4,26 @@ class PokemonController < ApplicationController
   end
 
   def show
-    api_response = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/")
-    body = JSON.parse(api_response.body)
+    test = "http://pokeapi.co/api/v2/pokemon/#{params[:id]}/" #the search for id/name is going on the server side
+    api_response_pokemon = HTTParty.get(test)
+    pokemon_body = JSON.parse(api_response_pokemon.body)
     # name  = body["name"]
     # id = body["id"]
     # types = body["types"]
+
+    api_response_giphy = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_KEY"]}&q=pikachu")
+    giphy_body = JSON.parse(api_response_giphy.body)
+    # binding.pry
+
 
     respond_to do |format|
       format.html #defaults to html, then json
       format.json do
         render json: {
-          name: body["name"],
-          id: body["id"],
-          types: body["types"]
+          name: pokemon_body["name"],
+          id: pokemon_body["id"],
+          types: pokemon_body["types"][0]["type"]["name"],
+          giphy_url: giphy_body["data"][0]["url"]
         }
       end
     end
